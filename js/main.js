533af6cdc415137f40becbd674adc33aefe39d27
Alexander Babin <a.babin@cti.ru>
2015-12-16 16:51:58 +0300

    comit

--- js/main.js
+++ js/main.js
@@ -1,7 +1,19 @@
 $(function(){
 	
+	// AJAX settings ------------------------------------------------------------------------
+
 	// отключаем кэширование запросов
 	$.ajaxSetup({ cache: false });
+	
+	// визуализация
+	$(document).ajaxSend(function(event, request, settings) {
+	    $('#loading-indicator').show();
+	});
+
+	$(document).ajaxComplete(function(event, request, settings) {
+	    $('#loading-indicator').hide();
+	});
+	// ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 	
 	// создаем левое меню
 	leftMenuView = new LeftMenuView();
@@ -14,10 +26,6 @@ $(function(){
 	
 	// Start Backbone history a necessary step for bookmarkable URL's
 	Backbone.history.start();
-	
-	
-	
-	
 	
 	
 });
@@ -57,6 +65,41 @@ var gridManager = {
 	}
 };
 
+
+DialogManager = function(){
+	
+	// mode can be follows: R - read , M-modify,  
+	
+	DialogManager.prototype.showDialogFor = function(grid, recId){
+		model = grid.collection.at(recId);
+		setHeader('Window header for ' + grid.component);
+		setContent('hello !!!');
+		showModalWindow();
+	};
+	
+	DialogManager.prototype.showDialogForRead = function(grid, recId){
+		model = grid.collection.at(recId);
+		setHeader('Window header for ' + grid.component);
+		setContent('hello !!!');
+		showModalWindow();
+	};
+	
+	setHeader = function(data){
+		var header =  $('#modalWindow div.modal-dialog div.modal-content div.modal-header h4.modal-title');
+		header.html(data);
+	};
+	
+	setContent = function(data){
+		var content =  $('#modalWindow div.modal-dialog div.modal-content div.modal-body');
+		content.html(data);
+	};
+	
+	showModalWindow = function(){
+		$('#modalWindow').modal();
+	};
+	
+};
+var dialogManager = new DialogManager();
 
 
 
